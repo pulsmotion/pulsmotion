39f9ca360c0245d8015c302bb6af388ec8e74d7d
benedikt <benne@Benne-MBP.local>
2017-03-04 17:21:39 +0100

    Changed file formatting

--- server/server.js
+++ server/server.js
@@ -10,20 +10,20 @@ var runningPortNumber = process.env.PORT || 8080;
 // CORS
 var whitelist = ['http://127.0.0.1', 'http://localhost','http://127.0.0.1:8080', 'http://localhost:8080'];
 var corsOptions = {
-    origin: function(origin, callback){
-        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
-        callback(null, originIsWhitelisted);
-    }
+  origin: function(origin, callback){
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    callback(null, originIsWhitelisted);
+  }
 };
 app.use(cors(corsOptions));
 
 // Socket IO
 io.sockets.on('connection', function (socket) {
-    console.log('Connected succesfully to the socket ' + new Date());
-	io.sockets.emit('connected', { msg: "someone connected" } );
+  console.log('Connected succesfully to the socket ' + new Date());
+  io.sockets.emit('connected', { msg: "someone connected" } );
 });
 
 // Server listens on port
 server.listen(runningPortNumber, function(){
-    console.log('Listening on port ' + this.address().port);
+  console.log('Listening on port ' + this.address().port);
 });
